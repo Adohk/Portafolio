a806f38e3203875d8be0f986ee2bc3c0789eeebb
Adohk <bloodmessenger@gmail.com>
2017-04-26 02:46:08 -0300

    Add files via upload

--- content/js/site.js
+++ content/js/site.js
@@ -1,24 +1,32 @@
 $('.button-collapse').sideNav();
 
-$(document).ready(function() {
-    setTimeout(function () { $('.page-loader-wrapper').fadeOut(); }, 5);
+$(document).ready(function () {
     var movementStrength = 15;
     var height = movementStrength / $(".user-info").height();
     var width = movementStrength / $(".user-info").width();
-    $(".user-info").mousemove(function(e){
-        var pageX = e.pageX - ($(".user-info").width() / 2);
+    $(".user-info").mousemove(function (e) {
+        // var pageX = e.pageX - ($(".user-info").width() / 2);
+        // var pageY = e.pageY - ($(".user-info").height() / 2);
+        // var newvalueX = width * pageX / -1 - 5;
+        // var newvalueY = height * pageY / -1 - 5;
+        // $('.user-info').css("background-position", newvalueX + "px     " + newvalueY + "px");
         var pageY = e.pageY - ($(".user-info").height() / 2);
-        var newvalueX = width * pageX / -1 -5;
-        var newvalueY = height * pageY / -1 -5;
-        $('.user-info').css("background-position", newvalueX+"px     "+newvalueY+"px");
+        var newvalueY = height * pageY / -1 - 5;
+        $('.user-info').css("background-position", width + "px     " + newvalueY + "px");
     });
 
-    document.addEventListener('scroll', function(e){
-        var pageX = e.pageX - ($(".user-info").width() / 2);
+    document.addEventListener('scroll', function (e) {
         var pageY = e.pageY - ($(".user-info").height() / 2);
-        var newvalueX = width * pageX / -1 -10;
-        var newvalueY = height * pageY / -1 -10;
-        $('.user-info').css("background-position", newvalueX+"px     "+newvalueY+"px");
+        var newvalueY = height * pageY / -1 - 10;
+        $('.user-info').css("background-position", width + "px     " + newvalueY + "px");
     });
 
+    setTimeout(function () {
+        $('.page-loader-wrapper').fadeOut();
+    }, 5);
+
+    $('#samu').lightGallery({
+        thumbnail: true,
+        selector: 'a'
+    });
 });
