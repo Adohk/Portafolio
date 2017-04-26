81ac846f42aa46b10b2a96319e2db6409c508798
Adohk <bloodmessenger@gmail.com>
2017-04-26 11:09:03 -0300

    git

--- content/js/site.js
+++ content/js/site.js
@@ -1,15 +1,16 @@
 $('.button-collapse').sideNav();
 
+$(window).on("load", function() {
+    setTimeout(function () {
+        $('.page-loader-wrapper').fadeOut();
+    }, 10);
+});
+
 $(document).ready(function () {
     var movementStrength = 15;
     var height = movementStrength / $(".user-info").height();
     var width = movementStrength / $(".user-info").width();
     $(".user-info").mousemove(function (e) {
-        // var pageX = e.pageX - ($(".user-info").width() / 2);
-        // var pageY = e.pageY - ($(".user-info").height() / 2);
-        // var newvalueX = width * pageX / -1 - 5;
-        // var newvalueY = height * pageY / -1 - 5;
-        // $('.user-info').css("background-position", newvalueX + "px     " + newvalueY + "px");
         var pageY = e.pageY - ($(".user-info").height() / 2);
         var newvalueY = height * pageY / -1 - 5;
         $('.user-info').css("background-position", width + "px     " + newvalueY + "px");
@@ -20,10 +21,6 @@ $(document).ready(function () {
         var newvalueY = height * pageY / -1 - 10;
         $('.user-info').css("background-position", width + "px     " + newvalueY + "px");
     });
-
-    setTimeout(function () {
-        $('.page-loader-wrapper').fadeOut();
-    }, 5);
 
     $('#samu').lightGallery({
         thumbnail: true,
